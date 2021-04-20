92f6b1bda9b77fe2940255719d4270faa6f8a62f
Olle Nilsson <olle.nilsson@medieinstitutet.se>
2021-04-20 17:27:10 +0200

    header css

--- src/components/AppRoute.js
+++ src/components/AppRoute.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import Header from './Header';
-import { Route, BrowserRouter as Router } from "react-router-dom";
+import Header from './Header/Header';
+import "./Body.css";
+import {BrowserRouter as Router} from "react-router-dom";
 
 export default function AppRoute() {
     return (
         <>
             <Router>
+                <body>
                 <Header />
-
+                <div>
+                </div>
+                </body>
             </Router>
-
-
-
         </>
     )
 }
