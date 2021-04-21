76e9a93b47412e543bc57ce5bf85cc9339fef293
Olle Nilsson <olle.nilsson@medieinstitutet.se>
2021-04-21 17:52:39 +0200

    added login

--- src/components/AppRoute.js
+++ src/components/AppRoute.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header/Header';
 import Home from './LandingPage/Home';
 import CardList from './Card/CardList';
+import Login from './Login/Login';
 import BookingForm from './Booking/BookingForm';
 import "./Body.css";
 import { Route, BrowserRouter as Router } from "react-router-dom";
@@ -13,6 +14,7 @@ export default function AppRoute() {
                 <Header />
                 <Route path="/" exact component={Home} />
                 <Route path="/barber" exact component={CardList} />
+                <Route path="/login" exact component={Login} />
                 <Route path="/booking" component={BookingForm} />
             </Router>
         </body>
